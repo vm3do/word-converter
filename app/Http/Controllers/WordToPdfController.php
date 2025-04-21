b62e41246234533d16a9161a3e8eef47a1aa31d2
vm3do <medoavvadi@gmail.com>
2025-04-21 15:16:10 +0100

    fixed chinese is not readable problem

--- app/Http/Controllers/WordToPdfController.php
+++ app/Http/Controllers/WordToPdfController.php
@@ -33,7 +33,15 @@ class WordToPdfController extends Controller
             // Read the HTML content
             $htmlContent = file_get_contents($tempHtmlPath);
             
+            // Add custom CSS with font support for Chinese characters
             $customCss = '<style>
+                @font-face {
+                    font-family: "Noto Sans SC";
+                    src: url("https://fonts.googleapis.com/css2?family=Noto+Sans+SC&display=swap");
+                }
+                body {
+                    font-family: "Noto Sans SC", Arial, sans-serif;
+                }
                 table { 
                     width: 100%; 
                     border-collapse: collapse; 
@@ -48,18 +56,22 @@ class WordToPdfController extends Controller
                     box-sizing: border-box;
                 }
             </style>';
-
+            
+            // Add meta tag for UTF-8 encoding
+            $htmlContent = str_replace('<head>', '<head><meta charset="UTF-8">', $htmlContent);
             $htmlContent = str_replace('</head>', $customCss . '</head>', $htmlContent);
             
             // Configure Dompdf
             $options = new Options();
             $options->set('isHtml5ParserEnabled', true);
             $options->set('isPhpEnabled', true);
+            $options->set('isRemoteEnabled', true); // Enable remote resources for fonts
             $options->set('dpi', 150);
+            $options->set('defaultFont', 'Noto Sans SC');
             
             // Dompdf
             $dompdf = new Dompdf($options);
-            $dompdf->loadHtml($htmlContent);
+            $dompdf->loadHtml($htmlContent, 'UTF-8');
             $dompdf->setPaper('A4', 'portrait');
             $dompdf->render();
             
